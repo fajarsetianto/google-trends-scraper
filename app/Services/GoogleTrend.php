4a30168630f07fc310d4ea79e81f7e250a71a87a
fajar setianto <muhamad.fajar.setianto@gmail.com>
2020-07-04 11:17:40 +0700

    update

--- app/Services/GoogleTrend.php
+++ app/Services/GoogleTrend.php
@@ -77,10 +77,11 @@ class GoogleTrend extends GTrends{
             'proxy_host' => 'p.webshare.io',
             'proxy_user'=> 'edswurpo-rotate',
             'proxy_pass' => 'qh30oorwzasa',
-            'proxy_port' => '80',
+            'proxy_port' => '1080',
             'curloptions' => [
                 CURLOPT_COOKIEJAR => $cookieJar,
                 CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_PROXYTYPE => 7
                 // CURLOPT_PROXYTYPE => CURLPROXY_SOCKS5
             ],
             'maxredirects' => 10,
